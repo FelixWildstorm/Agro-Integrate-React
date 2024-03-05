9bf33df70834b9818302deeb7a7f7f152d3ea10c
FelixWildstorm <felixluna1992@gmail.com>
2024-03-05 02:03:09 -0800

    Utils

---
+++ src/utils/axiosInstance.js
@@ -0,0 +1,6 @@
+// config.js
+// Development API URL for testing
+export const baseURL = 'http://localhost:8000/api';
+
+// Production API URL
+// export const baseURL = 'https://agro-integrate.herokuapp.com/api';
