a0182755d3f4099ca0c50d8196aac873e55ca8ec
FelixWildstorm <felixluna1992@gmail.com>
2024-03-05 02:04:30 -0800

    Brand & Css

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Fragment } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-function App() {
-  const [count, setCount] = useState(0)
+// React Query 
+import { QueryClient, QueryClientProvider } from 'react-query';
 
+// Localization
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+
+// date-fns
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+
+// Layouts
+import Main from './layouts/Main';
+
+// Components
+import NotFound from './components/NotFound/NotFound';
+
+// Routes
+import protectedRoutes from './routes/ProtectedRoutes';
+import publicRoutes from './routes/PublicRoutes';
+
+// User Context
+import UserContextProvider from './context/UserContext';
+
+// React Query Client
+const queryClient = new QueryClient();
+
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+    <Fragment>
+      <QueryClientProvider client={queryClient}>
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+          <UserContextProvider>
+            <BrowserRouter>
+              <Routes>
+                {publicRoutes.map((route, index) => {
+                  return (
+                    <Route
+                      path={route.path}
+                      element={route.element}
+                      key={index}
+                    />
+                  )
+                })}
+                 <Route path="/" element={<Main />}>
+                  {protectedRoutes.map((route, index) => {
+                    return (
+                      <Route
+                        path={route.path}
+                        element={route.element}
+                        key={index}
+                      />
+                    )
+                  })}
+                </Route>
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </BrowserRouter>
+          </UserContextProvider>
+        </LocalizationProvider>
+      </QueryClientProvider>
+    </Fragment>
+  );
+};
