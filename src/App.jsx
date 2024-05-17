bd99d78cf94446d27867c6952457f47d2599eb4a
FelixWildstorm <felixluna1992@gmail.com>
2024-05-16 23:40:56 -0700

    Update Authentication

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 // React Query 
 import { QueryClient, QueryClientProvider } from 'react-query';
 
+// Material-UI
+import { Avatar, Box, AppBar, Link, Typography } from '@mui/material';
+
 // Localization
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 
@@ -31,38 +34,72 @@ const queryClient = new QueryClient();
 export default function App() {
   return (
     <Fragment>
-       <ThemeCustomization>
-      <QueryClientProvider client={queryClient}>
-        <LocalizationProvider dateAdapter={AdapterDateFns}>
-          <UserContextProvider>
-            <BrowserRouter>
-              <Routes>
-                {publicRoutes.map((route, index) => {
-                  return (
-                    <Route
-                      path={route.path}
-                      element={route.element}
-                      key={index}
-                    />
-                  )
-                })}
-                <Route path="/" element={<Landing />}>
-                {protectedRoutes.map((route, index) => {
-                  return (
-                    <Route
-                      path={route.path}
-                      element={route.element}
-                      key={index}
-                    />
-                  )
-                })}
-                </Route>
-                <Route path="*" element={<NotFound />} />
-              </Routes>
-            </BrowserRouter>
-          </UserContextProvider>
-        </LocalizationProvider>
-      </QueryClientProvider>
+      <ThemeCustomization>
+        <QueryClientProvider client={queryClient}>
+          <LocalizationProvider dateAdapter={AdapterDateFns}>
+            <UserContextProvider>
+              <Box className="body">
+                <Box sx={{ display: 'block' }} className="page-wrapper">
+                  <Box
+                    className="navbar w-nav"
+                  >
+                    <Box className="nav-container">
+                      <Link href="/" aria-current="page" className="brands w-nav-brand w--current">
+                          <Box sx={{ m: 3 }}>
+                            <img src="images/Logo.svg" loading="lazy" alt="Corporate Logo" className="image" />
+                          </Box>
+                      </Link>
+                    </Box>
+                  </Box>
+                  <Box className="service-section wf-section">
+                    <Box className="border-container">
+                      <Box className="main-container">
+                        <BrowserRouter>
+                          <Routes>
+                            {publicRoutes.map((route, index) => {
+                              return (
+                                <Route
+                                  path={route.path}
+                                  element={route.element}
+                                  key={index}
+                                />
+                              )
+                            })}
+                            <Route path="/" element={<Landing />}>
+                              {protectedRoutes.map((route, index) => {
+                                return (
+                                  <Route
+                                    path={route.path}
+                                    element={route.element}
+                                    key={index}
+                                  />
+                                )
+                              })}
+                            </Route>
+                            <Route path="*" element={<NotFound />} />
+                          </Routes>
+                        </BrowserRouter>
+                      </Box>
+                    </Box>
+                  </Box>
+                </Box>
+                <Box className="footer">
+                  <Box className="section wf-section">
+                    <Box className="border-container">
+                      <Box className="main-container footer-container">
+                        <Box sx={{ m: 3 }}>
+                          <Typography variant="body2" color="textSecondary" align="center">
+                            © {new Date().getFullYear()}, Agro Integrate
+                          </Typography>
+                        </Box>
+                      </Box>
+                    </Box>
+                  </Box>
+                </Box>
+              </Box>
+            </UserContextProvider>
+          </LocalizationProvider>
+        </QueryClientProvider>
       </ThemeCustomization>
     </Fragment>
   );
--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,29 +1,145 @@
-import React, { useState } from 'react';
+import { createContext, useCallback, useEffect, useReducer, useMemo } from 'react';
 
-export const UserContext = React.createContext();
+// AWS Amplify
+import { Auth } from 'aws-amplify';
 
-const UserContextProvider = ({ children }) => {
-  const [user, setUser] = useState({
-    userName: '',
-    fistName: '',
-    lastName: '',
-    emailAddress: '',
-    permissionsLevel: {
-      active: false,
-      admin: false,
-      superAdmin: false,
-    }
-  });
+var ActionType;
+(function (ActionType) {
+  ActionType['INITIALIZE'] = 'INITIALIZE';
+  ActionType['LOGIN'] = 'LOGIN';
+  ActionType['LOGOUT'] = 'LOGOUT';
+})(ActionType || (ActionType = {}));
 
-  const updateUser = (newUser) => {
-    setUser(newUser);
-  };
-
-  return (
-    <UserContext.Provider value={{ user, updateUser }}>
-      {children}
-    </UserContext.Provider>
-  );
+const initialUserState = {
+  isAuthenticated: false,
+  isInitialized: false,
+  user: null,
 };
 
-export default UserContextProvider;
+const handlers = {
+  INITIALIZE: (state, action) => {
+    const { isAuthenticated, user } = action.payload;
+
+    return {
+      ...state,
+      isAuthenticated,
+      isInitialized: true,
+      user
+    };
+  },
+  LOGIN: (state, action) => {
+    const { user } = action.payload;
+
+    return {
+      ...state,
+      isAuthenticated: true,
+      user
+    };
+  },
+  LOGOUT: (state) => ({
+    ...state,
+    isAuthenticated: false,
+    user: null
+  })
+};
+
+const reducer = (state, action) => (handlers[action.type]
+  ? handlers[action.type](state, action)
+  : state);
+
+export const AuthContext = createContext({
+  ...initialUserState,
+  signIn: () => Promise.resolve(),
+  signOut: () => Promise.resolve(),
+  confirmSignUp: () => Promise.resolve(),
+  resendSignUp: () => Promise.resolve(),
+  forgotPassword: () => Promise.resolve(),
+  forgotPasswordSubmit: () => Promise.resolve(),
+});
+
+export const AuthProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialUserState);
+
+  const initialize = useCallback(async () => {
+    try {
+      const user = await Auth.currentAuthenticatedUser();
+
+      dispatch({
+        type: ActionType.INITIALIZE,
+        payload: {
+          isAuthenticated: true,
+          user: {
+            id: user.sub,
+            email: user.attributes.email,
+            username: user.username,
+            cognito_uuid: user.attributes.cognito_uuid,
+          }
+        }
+      });
+    } catch (error) {
+      dispatch({
+        type: ActionType.INITIALIZE,
+        payload: {
+          isAuthenticated: false,
+          user: null
+        }
+      });
+    }
+  }, [dispatch]);
+
+  useEffect(() => {
+    initialize();
+  }, [initialize]);
+
+  const signIn = async ({ email, password }) => {
+    try {
+      await Auth.signIn(email, password);
+      await initialize();
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  const signOut = async () => {
+    try {
+      await Auth.signOut();
+      dispatch({ type: ActionType.LOGOUT });
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  const confirmSignUp = useCallback(async (username, code) => {
+    await Auth.confirmSignUp(username, code);
+  }, []);
+
+  const resendSignUp = useCallback(async (username) => {
+    await Auth.resendSignUp(username);
+  }, []);
+
+  const forgotPassword = useCallback(async (username) => {
+    await Auth.forgotPassword(username);
+  }, []);
+
+  const forgotPasswordSubmit = useCallback(async (username, code, newPassword) => {
+    await Auth.forgotPasswordSubmit(username, code, newPassword);
+  }, []);
+
+  const value = useMemo(() => ({
+    ...state,
+    signIn,
+    signOut,
+    confirmSignUp,
+    resendSignUp,
+    forgotPassword,
+    forgotPasswordSubmit,
+  }), [state, signIn, signOut, confirmSignUp, resendSignUp, forgotPassword, forgotPasswordSubmit]);
+
+  return (
+    <AuthContext.Provider value={value}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export default AuthProvider;
